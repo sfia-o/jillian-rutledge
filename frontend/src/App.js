6b96cc0fee9dc9d38732ca3009c09c9be3eb417b
sfia-o <sofia.vl.oliveira@gmail.com>
2024-06-03 15:01:39 -0700

    Connect backend to frontend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+  const [userData, setUserData] = useState({users: []});
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setUserData(data)
+      }
+    )
+  }, []);
+
+  console.log(userData);
+
   return (
     <div>
-      
+      {
+        userData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      }
     </div>
   )
 }
