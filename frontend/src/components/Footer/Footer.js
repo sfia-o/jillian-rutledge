ec4ff697bbf41cbd1760ccddb4f08f3cf0c04d86
sfia-o <sofia.vl.oliveira@gmail.com>
2024-06-11 15:56:56 -0700

    Create Footer

---
+++ frontend/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FooterContainer, SocialMediaLinks, RightsStatement } from './FooterElements'
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <SocialMediaLinks></SocialMediaLinks>
+      <RightsStatement>2020 Jillian Rutledge. All Rights Reserved</RightsStatement>
+      
+    </FooterContainer>
+  )
+}
+
+export default Footer
